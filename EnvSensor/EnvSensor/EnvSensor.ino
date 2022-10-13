85bcc2db29a3688622fea0313d69e0356f5ac34d
Zoltan Zemniczki <night21@gmail.com>
2022-10-13 07:49:08 +0200

    Env sensor project

---
+++ EnvSensor/EnvSensor/EnvSensor.ino
@@ -0,0 +1,117 @@
+#include <MQ7.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 4     // D2 - 4 Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+#define SERVER_IP "192.168.50.109"
+
+#define STASSID "Home"
+#define STAPSK "Mk21rt73Z1237"
+
+#define A_PIN A0
+#define VOLTAGE 5
+
+// init MQ7 device
+MQ7 mq7(A_PIN, VOLTAGE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+
+  WiFi.begin(STASSID, STAPSK);
+  int counter = 0;
+  while (WiFi.status() != WL_CONNECTED && counter < 30) {
+    delay(500);
+    Serial.print(".");
+    counter++;
+  }
+  Serial.println("");
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());  
+
+  Serial.println("Calibrating MQ7");
+  mq7.calibrate();    // calculates R0
+  Serial.println("Calibration done!");
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
+
+  float ppm = mq7.readPpm();
+  Serial.print("PPM = "); Serial.println(ppm);
+
+   if ((WiFi.status() == WL_CONNECTED)) {
+
+    WiFiClient client;
+    HTTPClient http;
+
+    Serial.print("[HTTP] begin...\n");
+    // configure traged server and url
+    http.begin(client, "http://" SERVER_IP ":8080");  // HTTP
+    http.addHeader("Content-Type", "application/json");
+
+    Serial.print("[HTTP] POST...\n");
+    // start connection and send HTTP header and body
+    int httpCode = http.POST("{\"Type\":\"DHT22\", \"Data\":\"T="+ String(t, 3) + ", H=" + String(h, 3) + ", PPM=" + String(ppm, 3) + "\"}");
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        const String& payload = http.getString();
+        Serial.println("received payload:\n<<");
+        Serial.println(payload);
+        Serial.println(">>");
+      }
+    } else {
+      Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  }
+}
